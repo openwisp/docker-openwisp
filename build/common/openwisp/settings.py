d4ac0ca9838e1408ed0afc2d596c3674c509ba3b
Ajay Tripathi <ajay39in@gmail.com>
2020-06-17 23:55:27 +0530

    [gsoc2020] Removed django-freeradius #133
    Related #133

--- build/common/openwisp/settings.py
+++ build/common/openwisp/settings.py
@@ -168,8 +168,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = '%s/static' % BASE_DIR
-MEDIA_ROOT = '%s/media' % BASE_DIR
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+PRIVATE_STORAGE_ROOT = os.path.join(MEDIA_ROOT, 'private')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
