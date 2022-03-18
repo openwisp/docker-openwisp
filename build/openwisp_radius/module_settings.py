cb4e26a04ff31684eb62c773ce2a9ba9d5eacd08
Federico Capoano <f.capoano@openwisp.io>
2022-03-18 16:27:36 -0300

    [change] Added django.contrib.humanize
    Needed for https://github.com/openwisp/openwisp-radius/pull/394

--- build/openwisp_radius/module_settings.py
+++ build/openwisp_radius/module_settings.py
@@ -4,6 +4,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     # all-auth
     'django.contrib.sites',
     'allauth',
