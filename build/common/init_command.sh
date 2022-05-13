907a28cac2b645a4fdecf5537929461c3764ba9f
Aryaman <aryamanz29@gmail.com>
2022-05-13 19:26:54 +0530

    [change] Changed geocoding check to explicit command #169
    Before the django-loci app was doing the geocoding check at startup but it had
    too many unintended consequences so it was changed to an explicit command
    called only in the dashboard container.
    
    Closes #169

--- build/common/init_command.sh
+++ build/common/init_command.sh
@@ -7,6 +7,9 @@ init_conf
 
 # Start services
 if [ "$MODULE_NAME" = 'dashboard' ]; then
+    if [ "$OPENWISP_GEOCODING_CHECK" = 'True' ]; then
+        python manage.py check --deploy --tag geocoding
+    fi
     python services.py database redis
     python manage.py migrate --noinput
     python load_init_data.py
