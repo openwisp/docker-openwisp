a6a18785e774fac04afcbac6b20e32a6762b69b0
Tomas Peterka <tomas@peterka.me>
2023-05-21 21:51:37 +0200

    Move PID files to /tmp to ease the permissions

--- images/common/init_command.sh
+++ images/common/init_command.sh
@@ -62,41 +62,41 @@ elif [ "$MODULE_NAME" = 'nginx' ]; then
 	nginx -g 'daemon off;'
 elif [ "$MODULE_NAME" = 'celery' ]; then
 	python services.py database redis dashboard
-	echo "Starting the 'default' celery worker"
-	celery -A openwisp worker -l ${DJANGO_LOG_LEVEL} --queues celery \
-		-n celery@%h --logfile /opt/openwisp/logs/celery.log \
-		--pidfile /opt/openwisp/celery.pid --detach \
-		${OPENWISP_CELERY_COMMAND_FLAGS}
 
 	if [ "$USE_OPENWISP_CELERY_NETWORK" = "True" ]; then
-		echo "Starting the 'network' celery worker"
+		echo "Starting detached 'network' celery worker"
 		celery -A openwisp worker -l ${DJANGO_LOG_LEVEL} --queues network \
 			-n network@%h --logfile /opt/openwisp/logs/celery_network.log \
-			--pidfile /opt/openwisp/celery_network.pid --detach \
+			--pidfile /tmp/celery_network.pid --detach \
 			${OPENWISP_CELERY_NETWORK_COMMAND_FLAGS}
 	fi
 
 	if [[ "$USE_OPENWISP_FIRMWARE" == "True" && "$USE_OPENWISP_CELERY_FIRMWARE" == "True" ]]; then
-		echo "Starting the 'firmware_upgrader' celery worker"
+		echo "Starting detached 'firmware_upgrader' celery worker"
 		celery -A openwisp worker -l ${DJANGO_LOG_LEVEL} --queues firmware_upgrader \
 			-n firmware_upgrader@%h --logfile /opt/openwisp/logs/celery_firmware_upgrader.log \
-			--pidfile /opt/openwisp/celery_firmware_upgrader.pid --detach \
+			--pidfile /tmp/celery_firmware_upgrader.pid --detach \
 			${OPENWISP_CELERY_FIRMWARE_COMMAND_FLAGS}
 	fi
 	sleep 1s
+
+	echo "Starting the 'default' celery worker"
+	celery -A openwisp worker -l ${DJANGO_LOG_LEVEL} --queues celery \
+		-n celery@%h --logfile /opt/openwisp/logs/celery.log ${OPENWISP_CELERY_COMMAND_FLAGS}
 	tail -f /opt/openwisp/logs/*
+
 elif [ "$MODULE_NAME" = 'celery_monitoring' ]; then
 	python services.py database redis dashboard
 	if [[ "$USE_OPENWISP_MONITORING" == "True" && "$USE_OPENWISP_CELERY_MONITORING" == 'True' ]]; then
 		echo "Starting the 'monitoring' celery worker"
 		celery -A openwisp worker -l ${DJANGO_LOG_LEVEL} --queues monitoring \
 			-n monitoring@%h --logfile /opt/openwisp/logs/celery_monitoring.log \
-			--pidfile /opt/openwisp/celery_monitoring.pid --detach \
+			--pidfile /tmp/celery_monitoring.pid --detach \
 			${OPENWISP_CELERY_MONITORING_COMMAND_FLAGS}
 		echo "Starting the 'monitoring_checks' celery worker"
 		celery -A openwisp worker -l ${DJANGO_LOG_LEVEL} --queues monitoring_checks \
 			-n monitoring_checks@%h --logfile /opt/openwisp/logs/celery_monitoring_checks.log \
-			--pidfile /opt/openwisp/celery_monitoring_checks.pid --detach \
+			--pidfile /tmp/celery_monitoring_checks.pid --detach \
 			${OPENWISP_CELERY_MONITORING_CHECKS_COMMAND_FLAGS}
 		sleep 1s
 		tail -f /opt/openwisp/logs/*
@@ -104,7 +104,6 @@ elif [ "$MODULE_NAME" = 'celery_monitoring' ]; then
 		echo "Monitoring queues are not activated, exiting."
 	fi
 elif [ "$MODULE_NAME" = 'celerybeat' ]; then
-	rm -rf celerybeat.pid
 	python services.py database redis dashboard
 	celery -A openwisp beat -l ${DJANGO_LOG_LEVEL}
 else
