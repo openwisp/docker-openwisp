4830250a1d17bcbf5b776264defb95078e2ba13d
Gagan Deep <pandafy.dev@gmail.com>
2024-11-27 22:34:52 +0530

    [fix] Fixed __openwisp_version__
    Fixed publish directive in Makefile

--- images/common/openwisp/__init__.py
+++ images/common/openwisp/__init__.py
@@ -3,5 +3,5 @@ from __future__ import absolute_import, unicode_literals
 from .celery import app as celery_app
 
 __all__ = ['celery_app']
-__openwisp_version__ = '24.0.0a'
+__openwisp_version__ = '24.11.1'
 __openwisp_installation_method__ = 'docker-openwisp'
