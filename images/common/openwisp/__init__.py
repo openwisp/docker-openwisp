ba92bc7553f4ff108277c34114b2bb43b6fe2256
CodingWithSaksham <sakshamthecoder@gmail.com>
2026-05-20 12:20:37 +0530

    [fix] Duplicate assignment of `__openwisp__version__`

--- images/common/openwisp/__init__.py
+++ images/common/openwisp/__init__.py
@@ -8,7 +8,6 @@ __all__ = ["celery_app"]
 __openwisp_version__ = os.environ.get("OPENWISP_VERSION")
 __openwisp_installation_method__ = "docker-openwisp"
 
-__openwisp_version__ = os.environ.get("OPENWISP_VERSION")
 if __openwisp_version__ is None or __openwisp_version__ == "unknown":
     raise RuntimeError(
         "OPENWISP_VERSION not set in image. "
