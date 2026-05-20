a495a8fab5e347d94ddd36402ec96ceabbe83f13
CodingWithSaksham <sakshamthecoder@gmail.com>
2026-05-20 12:07:38 +0530

    [fix] Ensure OPENWISP_VERSION build arg is passed to openwisp-base image
    The base-build target was not passing --build-arg OPENWISP_VERSION to the
    final docker build command, causing openwisp-base to always be built with
    OPENWISP_VERSION=edge regardless of the RELEASE_VERSION set in the Makefile.
    
    This meant that `make release` could build and tag images as
    `openwisp-base:25.10.3` while the container internally reported version "edge",
    causing version mismatch and confusion in support scenarios.

--- images/common/openwisp/__init__.py
+++ images/common/openwisp/__init__.py
@@ -5,5 +5,12 @@ import os
 from .celery import app as celery_app
 
 __all__ = ["celery_app"]
-__openwisp_version__ = os.environ.get("OPENWISP_VERSION", "dev")
+__openwisp_version__ = os.environ.get("OPENWISP_VERSION")
 __openwisp_installation_method__ = "docker-openwisp"
+
+__openwisp_version__ = os.environ.get("OPENWISP_VERSION")
+if __openwisp_version__ is None or __openwisp_version__ == "unknown":
+    raise RuntimeError(
+        "OPENWISP_VERSION not set in image. "
+        "This indicates a broken build or image pull."
+    )
